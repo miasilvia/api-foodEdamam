e983c3ec49707d68e3e5696446e4983fa0434d70
miasilvia <135966912+miasilvia@users.noreply.github.com>
2024-03-09 15:07:15 +0700

    add animation

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 2s ease-in forwards',
+        'slide-down': 'slideDown 2s ease-in forwards'
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' }
+        },
+        slideDown: {
+          '0%': { opacity: '0', transform: 'translateY(-5%)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' }
+        }
+      }
+    },
+  },
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    // require('@tailwindcss/line-clamp'), // <-- Hapus atau komentari baris ini
+  ],
+};
