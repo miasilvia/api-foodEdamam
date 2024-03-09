d093aadf93ef5e10f699addaee8767120141ff1f
miasilvia <135966912+miasilvia@users.noreply.github.com>
2024-03-09 18:34:30 +0700

    two

--- src/pages/Food.jsx
+++ src/pages/Food.jsx
@@ -16,7 +16,7 @@ export default function Food({ foodData }) {
                     <div className="font-bold text-xl mb-2">
                       {food.food.label}
                     </div>
-                    <p className=" text-base ">Lorem ipsum dolor sit amett</p>
+                    <p className=" text-base ">Lorem ipsum dolor sit amettttt</p>
                   </div>
                   <div className="px-6 pt-4 pb-2">
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
